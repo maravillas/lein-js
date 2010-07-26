86b877132cd3227b2cee4cf2b82470b2f7d80930
Matthew Maravillas <maravillas@gmail.com>
2010-07-26 15:16:57 -0400

    Clean up project dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject lein-js "0.1.0-SNAPSHOT"
   :description "A Leiningen plugin for concatenating and compiling JavaScript files using Google's Closure Compiler. "
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-		     [swank-clojure "1.2.1"]]
-  :repositories [["clojars" "http://clojars.org/repo"]])
+  :dev-dependencies [[org.clojure/clojure "1.2.0-beta1"]
+		     [org.clojure/clojure-contrib "1.2.0-beta1"]
+		     [lein-clojars "0.5.0-SNAPSHOT"]
+		     [swank-clojure "1.2.1"]])
